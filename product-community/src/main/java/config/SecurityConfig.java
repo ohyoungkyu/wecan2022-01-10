5b1cc26f28853339c4b0d83568560db8afcea805
ohyoungkyu <come8908@gmail.com>
2022-01-14 14:51:23 +0900

    DB연결 및 SecurityConfig세팅

---
+++ product-community/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.persistence.Enumerated;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        web.ignoring().mvcMatchers("/css/**", "/js/**","/img/**","/error/**","/lib/**");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        http
+                .authorizeRequests( authorize -> authorize
+
+                        .mvcMatchers(
+                                "/members/join"
+                        )
+                        .anonymous()
+                        .mvcMatchers(
+                                "/articles/**"
+                        )
+                        .permitAll()
+                        .mvcMatchers(
+                                "/adm/**"
+                        )
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .denyAll()
+                )
+
+                .formLogin()
+                .loginPage("/members/login")
+                .loginProcessingUrl("/doLogin")
+                .usernameParameter("loginId")
+                .passwordParameter("loginPw")
+                .defaultSuccessUrl("/")
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .clearAuthentication(true)
+                .and()
+                .sessionManagement()
+                .invalidSessionUrl("/")
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .expiredUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ product-community/src/test/java/com/oyk/product/community/ProductCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyk.product.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
