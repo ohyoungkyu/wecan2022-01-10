d4685d216fd2ae31cf121f708e8d206f999e66bb
ohyoungkyu <come8908@gmail.com>
2022-02-07 15:48:58 +0900

    회원가입 메세지 랜더링

--- product-community/src/main/java/com/oyk/product/community/dto/member/MemberSaveForm.java
+++ product-community/src/main/java/com/oyk/product/community/dto/member/MemberSaveForm.java
@@ -7,15 +7,15 @@ import javax.validation.constraints.NotBlank;
 public class MemberSaveForm {
     // 회원가입시 필요한 정보
 
-    @NotBlank
+    @NotBlank(message = "아이디를 입력해주세요")
     private String loginId;
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String loginPw;
-    @NotBlank
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
-    @NotBlank
+    @NotBlank(message = "닉네임을 입력해주세요")
     private String nickname;
-    @NotBlank
+    @NotBlank(message = "이메일을 입력해주세요")
     private String email;
 
 }
