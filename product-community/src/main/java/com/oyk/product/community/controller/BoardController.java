f2315c060b427d5fbacf14e56b8ba7b5b36321bb
ohyoungkyu <come8908@gmail.com>
2022-02-09 15:28:57 +0900

    게시판 생성에러메세지 렌더링

--- product-community/src/main/java/com/oyk/product/community/controller/BoardController.java
+++ product-community/src/main/java/com/oyk/product/community/controller/BoardController.java
@@ -10,6 +10,8 @@ import com.oyk.product.community.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +36,11 @@ public class BoardController {
 
     }
     @PostMapping("/adm/boards/add")
-    public String doAddBoard(BoardSaveForm boardSaveForm, Principal principal){
+    public String doAddBoard(@Validated BoardSaveForm boardSaveForm, BindingResult bindingResult, Principal principal){
+
+        if(bindingResult.hasErrors()) {
+            return "adm/board/add";
+        }
 
         Member findAdmin = memberService.findByLoginId(principal.getName());
 
--- product-community/src/main/java/com/oyk/product/community/dto/board/BoardSaveForm.java
+++ product-community/src/main/java/com/oyk/product/community/dto/board/BoardSaveForm.java
@@ -2,9 +2,12 @@ package com.oyk.product.community.dto.board;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class BoardSaveForm {
 
+    @NotBlank(message = "게시판 이름을 입력해 주세요")
     private String name;
 
     private String detail;
