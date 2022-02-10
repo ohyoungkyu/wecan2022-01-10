a14d7b65753c34809a92cf0e0113a28f4a29612c
ohyoungkyu <come8908@gmail.com>
2022-02-10 15:48:33 +0900

    회원 관리 페이지, 회원 상세보기 로직 구현

--- product-community/src/main/java/com/oyk/product/community/controller/AdmMemberController.java
+++ product-community/src/main/java/com/oyk/product/community/controller/AdmMemberController.java
@@ -1,11 +1,13 @@
 package com.oyk.product.community.controller;
 
+import com.oyk.product.community.dto.adm.AdmMemberDetailDTO;
 import com.oyk.product.community.dto.member.MemberDTO;
 import com.oyk.product.community.service.AdmMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +27,17 @@ public class AdmMemberController {
         model.addAttribute("members", members);
 
         return "adm/member/main";
+    }
+
+    @GetMapping("/members/detail/{id}")
+    public String showMemberDetail(@PathVariable(name= "id") Long id , Model model){
+
+        AdmMemberDetailDTO memberDetail = admMemberService.getMemberDetail(id);
+
+        model.addAttribute("member", memberDetail);
+
+        return "adm/member/detail";
+
     }
 
 }
--- product-community/src/main/java/com/oyk/product/community/service/AdmMemberService.java
+++ product-community/src/main/java/com/oyk/product/community/service/AdmMemberService.java
@@ -1,13 +1,17 @@
 package com.oyk.product.community.service;
 
 import com.oyk.product.community.dao.MemberRepository;
+import com.oyk.product.community.domain.Article;
 import com.oyk.product.community.domain.Member;
+import com.oyk.product.community.dto.adm.AdmMemberDetailDTO;
+import com.oyk.product.community.dto.artcle.ArticleListDTO;
 import com.oyk.product.community.dto.member.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +31,31 @@ public class AdmMemberService {
         }
 
         return memberDTOList;
+
+    }
+
+    public AdmMemberDetailDTO getMemberDetail(Long id){
+
+        List<ArticleListDTO> articleDTOList = new ArrayList<>();
+
+        Optional<Member> memberOptional = memberRepository.findById(id);
+
+        memberOptional.orElseThrow(
+                () -> new IllegalStateException("존재하지 않는 회원입니다.")
+        );
+
+        Member findMember = memberOptional.get();
+
+        List<Article> articles = findMember.getArticles();
+
+        for( Article article : articles ) {
+
+            ArticleListDTO articleListDTO = new ArticleListDTO(article);
+
+            articleDTOList.add(articleListDTO);
+
+        }
+        return new AdmMemberDetailDTO(findMember, articleDTOList);
 
     }
 
