baf99b975a3706cf7c376be0f15a519eb762935e
ohyoungkyu <come8908@gmail.com>
2022-03-08 14:50:33 +0900

    마이페이지 ui1차

--- product-community/src/main/java/com/oyk/product/community/service/MemberService.java
+++ product-community/src/main/java/com/oyk/product/community/service/MemberService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -111,6 +112,8 @@ public class MemberService implements UserDetailsService {
         Member findMember = findByLoginId(loginId);
 
         List<Article> articles = findMember.getArticles();
+
+        Collections.reverse(articles);
 
         for( Article article : articles) {
 
